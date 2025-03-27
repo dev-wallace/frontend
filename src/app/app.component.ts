fc76b0ef147a202e86047513b329f037e30f254a
yokaithefato <wallacewwc2015wwk@gmail.com>
2025-03-26 22:56:10 -0300

    refactor: update app layout and styles, add Angular Material support

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AdicionarEditarUsuarioComponent } from './components/adicionar-editar-usuario/adicionar-editar-usuario.component'; // Importando o componente AdicionarEditarUsuario
+import { ListUsuariosComponent } from './components/list-usuarios/list-usuarios.component'; // Importando o componente ListUsuarios
+import { MatToolbarModule } from '@angular/material/toolbar';  
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [RouterOutlet, AdicionarEditarUsuarioComponent, ListUsuariosComponent,MatToolbarModule], // Adicionando os componentes Standalone
+  templateUrl: './app.component.html', // Usando o template no arquivo HTML separado
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'frontend-barbershop';
